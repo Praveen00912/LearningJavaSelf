7ff2199245ab2f2956d21cb4ed8d9f077c4b428c
Praveen00912 <praveenkumarreddy2627@gmail.com>
2026-02-20 10:22:39 +0530

    added SumofDigits

---
+++ SumofDigits.java
@@ -0,0 +1,14 @@
+public class SumofDigits {
+    public static void main(String[] args) {
+        int num = 894563;
+        int sum = 0;
+
+        while (num > 0) {
+            int digit = num % 10;
+            sum += digit;
+            num = num / 10;
+        }
+
+        System.out.println("Sum of digits = " + sum);
+    }
+}
