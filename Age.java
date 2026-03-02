967f6bdef7a1d51447436b9e0eb26165f7e5c017
Praveen00912 <praveenkumarreddy2627@gmail.com>
2026-03-02 20:46:30 +0530

    added

---
+++ Age.java
@@ -0,0 +1,11 @@
+/*class Age{
+    public static void main(String[] args) {
+        int age=25;
+        boolean hasLicense= true;
+        boolean result= (age<30)&& hasLicense;
+
+        boolean result1 = !hasLicense;
+        System.out.println(result1);
+        boolean result2= (age<10)|| hasLicense;
+    }
+}*/
