a28a32c8d08c7f03d293b8d2d5702834f8a3255e
Praveen00912 <praveenkumarreddy2627@gmail.com>
2026-03-02 20:47:30 +0530

    added

---
+++ IncDecOperators.java
@@ -0,0 +1,8 @@
+class IncDecOperators{
+    public static void main(String[] args) {
+        int var1=10;
+        int var2=--var1;
+        System.out.println(var2);
+        System.out.println(var1);
+    }
+}
